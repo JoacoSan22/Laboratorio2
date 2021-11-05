338b3fe42bce60ab3d77aa03d65f4f39af690ea5
JoacoSan22 <93724393+JoacoSan22@users.noreply.github.com>
2021-11-04 23:25:43 -0300

    Add files via upload

---
+++ Herencia-clase13/InmobiliariaForms/Local.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InmobiliariaForms
+{
+    class Local : Inmueble
+    {
+
+        public double local(double preFinal, int anios, int metrosCuadrad, int numVentanas)
+        {
+            
+            
+            if (anios < 15)
+            {
+                preFinal -= (preFinal * 0.01);
+            }
+            else
+            {
+                preFinal -= (preFinal * 0.02);
+            }
+            if (metrosCuadrad > 50)
+            {
+                preFinal += (preFinal * 0.01);
+            }
+            if (numVentanas <= 1)
+            {
+                preFinal -= (preFinal * 0.02);
+            }
+            else if (numVentanas > 4)
+            {
+                preFinal += (preFinal * 0.02);
+            }
+            return preFinal;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Herencia-clase13/InmobiliariaForms/Piso.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InmobiliariaForms
+{
+    public class Piso : Inmueble
+    {
+        public double ingresarPiso(double preFinal, int numPiso, int anios)
+        {
+            if (anios < 15)
+            {
+                preFinal -= (preFinal * 0.01);
+            }
+            else
+            {
+                preFinal -= (preFinal * 0.02);
+            }
+            if (numPiso >= 3)
+            {
+                preFinal += (preFinal * 0.03);
+            }
+            return preFinal;
+        }
+
+        
+
+        
+    }
+}
